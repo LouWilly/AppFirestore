505eb32fe4c6c1f06c8e53c05a912dbc540d4816
LouWilly <mllf.flor@gmail.com>
2022-09-26 22:42:01 -0300

    firestore

---
+++ app/src/main/java/com/example/appfirestore/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.appfirestore
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val edtNome: EditText = findViewById(R.id.edtNome)
+        val edtEndereco: EditText = findViewById(R.id.edtEndereco)
+        val edtBairro: EditText = findViewById(R.id.edtBairro)
+        val edtCep: EditText = findViewById(R.id.edtCep)
+        val btnCadastrar: Button = findViewById(R.id.btnCadastrar)
+
+        // Access a Cloud Firestore instance from your Activity
+        val db = Firebase.firestore
+
+        btnCadastrar.setOnClickListener {
+            // Create a new user with a first and last name
+            val cadastroPessoa = hashMapOf(
+                "nome" to edtNome.text.toString(),
+                "endereco" to edtEndereco.text.toString(),
+                "bairro" to edtBairro.text.toString(),
+                "cep" to edtCep.text.toString()
+            )
+
+            // Add a new document with a generated ID
+            db.collection("cadastro")
+                .add(cadastroPessoa)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
+        }
+    }
+}
